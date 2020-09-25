1bbc605cbfa96cd80bb5a4ec4dc0d56f16376230
hyungraelee <70879607+hyungraelee@users.noreply.github.com>
2020-09-25 22:41:17 +0900

    first commit for 42cursus subject

---
+++ libft/ft_memset.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void        *ft_memset(void *ptr, int c, size_t n)
+(
+    unsigned char   *newPtr;
+    unsigned char   src;
+    size_t i;
+
+    newPtr = ptr;
+    src = c;
+    i = 0;
+    while (i++ < n)
+        *newPtr++ = src;
+    return (ptr);
+)
