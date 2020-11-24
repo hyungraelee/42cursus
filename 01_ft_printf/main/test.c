5a5c117241e8ab7d544511ab7cbb25775a47aca3
hyungraelee <timgg12345@gmail.com>
2020-11-25 02:00:55 +0900

    20201124 ft_printf

--- 01_ft_printf/main/test.c
+++ 01_ft_printf/main/test.c
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:46:59 by hyunlee           #+#    #+#             */
-/*   Updated: 2020/11/24 01:11:07 by hyunlee          ###   ########.fr       */
+/*   Updated: 2020/11/24 19:00:26 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main()
 {
 	char	*str = "%.3e'";
 	char	*str_num = "0.0";
-	double	num = -0.5;
+	double	num = 9.9996;
 
 	printf("\n==========================================\n");
 	printf("Result of \"%s\" for ", str); printf("\"%s\"\n", str_num);
