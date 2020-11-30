7d3c8286aeef9bc9498fc0fdb45e76cca56f8106
hyungraelee <timgg12345@gmail.com>
2020-11-30 18:32:28 +0900

    20201130 ft_printf

--- 01_ft_printf/main/main.c
+++ 01_ft_printf/main/main.c
@@ -72,32 +72,3 @@ ft_printf("%e", 10.12);
 	// printf("\n%f", __DBL_MAX__);
 	return 0;
 }
-
-
-// Test 4034 (bonus_e_rnd_half_to_even_02) : FAILED.
-//     First line of code: {return ft_printf("%.0e", -4.5);
-//       expected output : "-4e+00"
-//       your output     : "-5e+00"
-//       expected (nonprintable as hex) : "-4e+00"
-//       actual   (nonprintable as hex) : "-5e+00"
-
-// Test 4036 (bonus_e_rnd_half_to_even_04) : FAILED.
-//     First line of code: {return ft_printf("%.0e", -2.5);
-//       expected output : "-2e+00"
-//       your output     : "-3e+00"
-//       expected (nonprintable as hex) : "-2e+00"
-//       actual   (nonprintable as hex) : "-3e+00"
-
-// Test 4041 (bonus_e_rnd_half_to_even_09) : FAILED.
-//     First line of code: {return ft_printf("%.0e", 2.5);
-//       expected output : "2e+00"
-//       your output     : "3e+00"
-//       expected (nonprintable as hex) : "2e+00"
-//       actual   (nonprintable as hex) : "3e+00"
-
-// Test 4043 (bonus_e_rnd_half_to_even_11) : FAILED.
-//     First line of code: {return ft_printf("%.0e", 4.5);
-//       expected output : "4e+00"
-//       your output     : "5e+00"
-//       expected (nonprintable as hex) : "4e+00"
-//       actual   (nonprintable as hex) : "5e+00"
